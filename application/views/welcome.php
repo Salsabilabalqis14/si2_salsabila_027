a1c0dd102f4116e05894a140503611ac23cadcbf
Salsabilabalqis14 <salsabilabalqis1012@gmail.com>
2022-03-27 10:17:16 +0700

    first commit

---
+++ application/views/welcome.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Belajar CI</title>
+    </head>
+    <body>
+        <h1>Hello Lab 2</h1>
+        <h3>Nama : <?php echo $nama?></h3>
+        <h3>NIM : <?php echo $nim?></h3>
+        <h3>KOM : <?php echo $kom?></h3>
+    </body>
+</html>
